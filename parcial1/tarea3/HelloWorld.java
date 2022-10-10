cfba622d16fdf73104667392dc6606ef5d531806
JoseAngel26 <zs22022253@estudiantes.uv.mx>
2022-10-10 00:09:58 -0500

    se agrego holamundo.java

---
+++ parcial1/tarea3/HelloWorld.java
@@ -0,0 +1,10 @@
+// HelloWorld.java
+// Author: Jose Angel
+// Date: sep 26, 2022
+
+class HelloWorld {
+   public static void main(String[] args) {
+       System.out.println("Hello, World !"); 
+   }
+} 
+
